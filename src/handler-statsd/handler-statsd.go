e75d87c0fb98839930fd5140af3366a18f0859a0
Matt Jones <mattjones@yieldbot.com>
2015-12-05 22:02:11 -0500

    add statsd client configs

--- src/handler-statsd/handler-statsd.go
+++ src/handler-statsd/handler-statsd.go
@@ -1,23 +1,40 @@
+// Take well-formed input in graphie format and send it to statsd
 //
-//
-//
-// #! /usr/bin/env ruby
-//
-// require 'sensu-handler'
-// require 'socket'
-// require 'json'
-//
-// class StatsdEvent < Sensu::Handler
-//
-//   input = JSON.parse(STDIN.read)
-//   data = input['check']['output']
-//   server = ''
-//   port = 8125
-//
-//   s = UDPSocket.new
-//   s.connect(server, port)
-//
-//   data.each_line do |metric|
-//     s.send(metric, 0)
-//   end
-// end
+// LICENSE:
+//   Copyright 2015 Yieldbot. <devops@yieldbot.com>
+//   Released under the MIT License; see LICENSE
+//   for details.
+
+package main
+
+import (
+	"flag"
+	dracky "github.com/yieldbot/sensu-yieldbot-library/src"
+  "github.com/quipo/statsd"
+)
+
+func main() {
+
+  // set commandline flags
+	HostPtr := flag.String("host", "localhost" "the statsd host")
+	PortPtr := flag.String("port", "8125", "the statsd port")
+  ProtocolPtr := flag.String("protocol", "udp", "the protocol to connect with")
+
+	flag.Parse()
+	statsdHost := *HostPtr
+	statsdPort := *PortPtr
+  statsdProtocol := *ProtocolPtr
+
+   statsdclient := statsd.NewStatsdClient(statsdHost + ":" + statsdPort)
+   statsdclient.CreateSocket()
+   defer stats.Close()
+
+   sensuEvent := new(dracky.SensuEvent)
+   sensuEvent = sensuEvent.AcquireSensuEvent()
+
+   metricData := sensuEvent.Check.Output
+
+   for i, m := range metricData {
+     statsdclient.SesdEvent(m)
+   }
+ }
